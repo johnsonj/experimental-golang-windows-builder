65e1126d65fe5f42b6c51bd575bd3f131a98b2e6
Jeff Johnson <jrjohnson@google.com>
2017-03-24 14:50:45 -0700

    need to create credentials for the buildlet to start
    Without that the scheduled task never seems to start. Creating the
    credentials and reseting the VM give us a responsive buildlet.

--- rebuild.sh
+++ rebuild.sh
@@ -9,10 +9,23 @@ rm -f instance.txt
 # Create resources
 terraform apply --var "projectid=${PROJECT_ID}" --var "zone=${ZONE}"
 
-echo "Waiting 80 seconds to fetch logs, Ctrl+C if you aren't interested"
-sleep 80
+echo "Waiting 120 seconds for sysprep to finish"
+sleep 120
 
 echo ""
 echo "Start logs:"
 echo ""
 gcloud compute instances get-serial-port-output buildlet-win2016 --zone="${ZONE}" --project="${PROJECT_ID}" 
+
+# Set, fetch credentials
+yes "Y" | gcloud compute reset-windows-password buildlet-win2016 --user wingopher --project="${PROJECT_ID}" --zone="${ZONE}" > instance.txt
+
+echo "Restarting the instance"
+gcloud compute instances reset buildlet-win2016 --zone="${ZONE}" --project="${PROJECT_ID}" 
+
+echo "Waiting 60 seconds for the instance to boot"
+sleep 60
+external_ip=$(gcloud compute instances describe buildlet-win2016 --project="${PROJECT_ID}" --zone="${ZONE}" --format="value(networkInterfaces[0].accessConfigs[0].natIP)")
+echo curl "http://${external_ip}"
+curl "http://${external_ip}"
+
