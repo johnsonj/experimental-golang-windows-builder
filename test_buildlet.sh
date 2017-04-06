98f8b4b3029ecc346b40d9a702902a332e5434d5
Jeff Johnson <jrjohnson@google.com>
2017-04-06 16:23:26 -0700

    Remove workaround for path issue, public gcc
    - use vanila go1.8, run short tests
    - uploaded gcc repackaging to gcs

--- test_buildlet.sh
+++ test_buildlet.sh
@@ -1,40 +1,15 @@
 #!/usr/bin/env bash
-
 set -e -u
 
 hostname="$(grep ip_address instance.txt | cut -d ':' -f 2 | xargs echo -n)"
 BUILDLET="windows-amd64-gce@${hostname}"
 
 echo "Pushing GCC, go1.4, go to buildlet"
-gomote puttar "$BUILDLET" gcc5-1/gcc.tar.gz
+gomote puttar -url https://storage.googleapis.com/godev/gcc5-1.tar.gz  "$BUILDLET" 
+gomote puttar -url https://storage.googleapis.com/golang/go1.8.src.tar.gz "$BUILDLET"
 gomote put14 "$BUILDLET"
 
-# TODO(jrjohnson): this version tries to run 'go.exe' in the the test dirs.
-#                  need to determine if it's a env issue or a bug.
-# gomote puttar -url https://storage.googleapis.com/golang/go1.8.src.tar.gz $BUILDLET
-#
-# workaround is to use this patch:
-#
-# diff --git a/src/cmd/dist/test.go b/src/cmd/dist/test.go
-# index c51dcead2b..8e61ba2774 100644
-# --- a/src/cmd/dist/test.go
-# +++ b/src/cmd/dist/test.go
-# @@ -657,6 +657,11 @@ func (t *tester) registerSeqTest(name, dirBanner, bin string, args ...string) {
-#  }
-# 
-#  func (t *tester) bgDirCmd(dir, bin string, args ...string) *exec.Cmd {
-# +       //TODO(jrjohnson): hack?
-# +       if bin == "go" {
-# +               bin = filepath.Join(t.goroot, "bin", bin)
-# +       }
-# +
-#         cmd := exec.Command(bin, args...)
-#         if filepath.IsAbs(dir) {
-#                 cmd.Dir = dir
-#
-gomote puttar "$BUILDLET" go.tar.gz
-
 echo "Building go"
-gomote run -path '$WORKDIR\gcc\bin,$PATH' "$BUILDLET" go/src/make.bat
+gomote run -path '$PATH,$WORKDIR/gcc/bin,$WORKDIR/go/bin,$PATH' -e 'GOROOT=c:\workdir\go' "$BUILDLET" go/src/make.bat
 echo "Running tests for go"
-gomote run -path '$WORKDIR\gcc\bin,$WORKDIR\go\bin,$PATH' "$BUILDLET" go/bin/go tool dist test
+gomote run -path '$WORKDIR/gcc/bin,$WORKDIR/go/bin,$PATH' -e 'GOROOT=C:\workdir\go' "$BUILDLET" go/bin/go.exe test cmd/go -short
