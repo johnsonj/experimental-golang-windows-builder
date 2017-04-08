86c8eef9bfb667a1b0be5140d59a280fb133df47
Jeff Johnson <jrjohnson@google.com>
2017-04-07 17:46:15 -0700

    handle the new inputs of build.sh, build in parellel

--- buildall.sh
+++ buildall.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-set -e
-set -u
+set -e -u
 
-images=( 
-         "windows-server-2016-dc-core-v20170214"
-         "windows-server-2008-r2-dc-v20170214" 
-         "windows-server-2012-r2-dc-core-v20170214"
+declare -A public_images
+
+public_images=( 
+         ['server-2016']='windows-server-2016-dc-core-v20170214'
+         ['server-2008']='windows-server-2008-r2-dc-v20170214' 
+         ['server-2012']='windows-server-2012-r2-dc-core-v20170214'
        )
 
-for image in "${images[@]}"; do
-    echo $image
-    IMAGE=${image} ./rebuild.sh
-    mkdir -p out
-    ./test_buildlet.sh |& tee out/${image}.txt
+mkdir -p out
+
+for image in "${!public_images[@]}"; do
+    prefix=$image
+    base_image=${public_images[$image]}
+
+    BASE_IMAGE="$base_image" IMAGE_PROJECT='windows-cloud' ./rebuild.sh "$prefix" |& tee "out/${base_image}.txt" &
 done
+
+
+wait
